53bc69e8e3e19770782994d8c6d327e722ee7995
Jeremy Comelli <jeremycomelli@bluewin.ch>
2022-03-13 08:40:26 +0100

    Added DrawLevel and ImportLevel functions

--- Assets/GameGrid.cs
+++ Assets/GameGrid.cs
@@ -6,12 +6,13 @@ public class GameGrid : MonoBehaviour {
 
     public int width;
     public int height;
-
+    
+    // Levels/Current level
     public List<string> LevelPaths;
-
     public List<GameObject[,]> levels;
+    public GameObject[,] CurrentLevelTiles;
 
-    public GameObject[,] tiles;
+    // Tile prefabs
     public List<GameObject> GrassTilesPrefabs;
     public GameObject DirtTilePrefab;
     public GameObject WaterTilePrefab;
@@ -25,7 +26,7 @@ public class GameGrid : MonoBehaviour {
     // Start is called before the first frame update
     void Start()
     {
-        tiles = new GameObject[width, height];
+        CurrentLevelTiles = new GameObject[width, height];
         //Instantiate all game tiles to default value (ground) 
         for (int i = 0; i < width; i++)
         {
@@ -46,7 +47,7 @@ public class GameGrid : MonoBehaviour {
 
 
 
-                tiles[i, j] = CurrentTile;
+                CurrentLevelTiles[i, j] = CurrentTile;
             }
         }
 
@@ -66,7 +67,7 @@ public class GameGrid : MonoBehaviour {
                 Vector2Int currPos = new Vector2Int(i, j) + GridPositionToArrayIndex(gridPosition);
                 if (0 <= currPos.x && currPos.x < 12 && 0 <= currPos.y && currPos.y < 12)
                 {
-                    tiles[currPos.x, currPos.y].GetComponent<TileScript>().SetState(TileScript.State.grass);
+                    CurrentLevelTiles[currPos.x, currPos.y].GetComponent<TileScript>().SetState(TileScript.State.grass);
                 }
             }
         }
@@ -75,7 +76,7 @@ public class GameGrid : MonoBehaviour {
     public void PlantLongSeed(Vector3Int gridPosition, Direction direction)
     {
         Vector2Int currPos = GridPositionToArrayIndex(gridPosition);
-        tiles[currPos.x, currPos.y].GetComponent<TileScript>().SetState(TileScript.State.grass);
+        CurrentLevelTiles[currPos.x, currPos.y].GetComponent<TileScript>().SetState(TileScript.State.grass);
 
         int delta;
         if (direction == Direction.up || direction == Direction.right) delta = 1;
@@ -87,7 +88,7 @@ public class GameGrid : MonoBehaviour {
             if (direction == Direction.up || direction == Direction.down) currPos += new Vector2Int(0, delta);
             else currPos += new Vector2Int(delta, 0);
 
-            tiles[currPos.x, currPos.y].GetComponent<TileScript>().SetState(TileScript.State.grass);
+            CurrentLevelTiles[currPos.x, currPos.y].GetComponent<TileScript>().SetState(TileScript.State.grass);
         }
     }
 
@@ -117,7 +118,42 @@ public class GameGrid : MonoBehaviour {
         for(int i = 0; i < LevelPaths.Count; i++)
         {
             // read Vickys enums here
+            GameObject[,] CurrentLevel;
 
+
+            // Remove
+            CurrentLevel = null;
+
+
+            levels.Add(CurrentLevel);
+        }
+    }
+
+    public void Draw_level(GameObject[,] newLevel)
+    {
+        CurrentLevelTiles = new GameObject[width, height];
+        //Instantiate all game tiles to default value (ground) 
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+
+                // Create empty gameObject since we need to dynamically swap the displayed gameObject; the tile is therefore a child set thorugh tileScript.SetObject(Gameobject object)
+                GameObject CurrentTile = new GameObject("Tile");
+                CurrentTile.AddComponent<TileScript>();
+                CurrentTile.GetComponent<TileScript>().gameGrid = this;
+
+                // Instantiate the Tile GameObject with an offset to prevent clipping with the underlying tilemap 
+                CurrentTile.GetComponent<TileScript>().SetObject(Instantiate(DirtTilePrefab, tileMap.GetCellCenterWorld(ArrayIndexToGridPosition(new Vector2Int(i, j))) + new Vector3(0, 0, -1), Quaternion.identity, CurrentTile.transform));
+
+                // Set default tile (dirt)
+                // DO NOT CALL BEFORE SETTING A TILE BECAUSE THEN MY SHITTY CODE BREAKS
+                CurrentTile.GetComponent<TileScript>().SetState(TileScript.State.dirt);
+
+
+
+                CurrentLevelTiles[i, j] = CurrentTile;
+            }
         }
     }
 }
