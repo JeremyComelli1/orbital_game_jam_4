06f204f6b86288bd1687b11fe5490dc7fe82802a
Schouwey <zaid.schouwey@gmail.com>
2022-03-13 01:05:23 +0100

    Draw arrows

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -2,29 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
-{
+{ 
     private int _longSeedNumber;
     private int _thiccSeedNumber;
     private TextMeshProUGUI _longSeed;
     private TextMeshProUGUI _thiccSeed;
+    private Button _longSeedButton;
+    private Button _thiccSeedButton;
+
+    public GameObject LongSeedButton;
+    public GameObject ThiccSeedButton;
     public GameObject longSeed;
     public GameObject thiccSeed;
+    public int selectedSeed { get; set; }
     // Start is called before the first frame update
     void Start()
     {
         _longSeedNumber = 0;
         _thiccSeedNumber = 0;
+        _longSeedButton = LongSeedButton.GetComponent<Button>();
+        _thiccSeedButton = ThiccSeedButton.GetComponent<Button>();
         _longSeed = longSeed.GetComponent<TextMeshProUGUI>();
         _thiccSeed = thiccSeed.GetComponent<TextMeshProUGUI>();
         _longSeed.text = _longSeedNumber.ToString();
         _thiccSeed.text = _thiccSeedNumber.ToString();
+        _longSeedButton.onClick.AddListener(delegate { SelectSeed(1); });
+        _thiccSeedButton.onClick.AddListener(delegate { SelectSeed(0); });
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void SelectSeed(int seedType)
+    {
+        selectedSeed = seedType;
     }
 }
--- Assets/TileMapController.cs
+++ Assets/TileMapController.cs
@@ -6,20 +6,44 @@ using UnityEngine.Tilemaps;
 public class TileMapController : MonoBehaviour
 {   
     public Tilemap tileMap;
-
+    public GameObject gameController; 
     public Tile highlightTile;
     public Tilemap highlightMap;
+    public GameObject ArrowButton;
+    public GameObject UI;
 
+    private GameController _gameController;
     private Vector3Int previous;
+    private GameObject arrowDown,arrowUp,arrowLeft,arrowRight;
+
+    void Start()
+    {
+        _gameController = gameController.GetComponent<GameController>();
+
+        
+    }
 
     void Update()
-    {
+    { 
         if(Input.GetMouseButtonDown(0))
         {
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            
             Vector3Int gridPosition = tileMap.WorldToCell(mousePosition);
 
+
+
+            
+            Vector3 tileCenterPosition = tileMap.GetCellCenterWorld(gridPosition);
+
+
+
+
             TileBase clickedTile = tileMap.GetTile(gridPosition);
+
+            // If it's a long seed
+            if(_gameController.selectedSeed == 0)
+                ShowDirectionnalArrows(tileCenterPosition);
 
             // if the position has changed
             if (gridPosition != previous)
@@ -35,5 +59,51 @@ public class TileMapController : MonoBehaviour
             }
 
         }
+    }
+    void ShowDirectionnalArrows(Vector3 mousePosition)
+    {
+        if (this.arrowRight)
+        {
+            Destroy(arrowRight);
+            Destroy(arrowLeft);
+            Destroy(arrowUp);
+            Destroy(arrowDown);
+        }
+
+
+
+
+        
+        Vector2 screenPoint = RectTransformUtility.WorldToScreenPoint(Camera.main, mousePosition);
+        
+        Vector2 anchoredPosition = transform.InverseTransformPoint(screenPoint);
+        Vector2 offset;
+        offset = new Vector2(-100,0);
+        anchoredPosition = anchoredPosition + offset;
+        arrowLeft = Instantiate(ArrowButton, anchoredPosition, Quaternion.identity);
+
+        anchoredPosition = transform.InverseTransformPoint(screenPoint);
+        offset = new Vector2(100, 0);
+        anchoredPosition = anchoredPosition + offset;
+        arrowRight = Instantiate(ArrowButton, anchoredPosition, Quaternion.identity);
+
+        anchoredPosition = transform.InverseTransformPoint(screenPoint);
+        offset = new Vector2(0, -100);
+        anchoredPosition = anchoredPosition + offset;
+        arrowDown = Instantiate(ArrowButton, anchoredPosition, Quaternion.identity);
+
+        anchoredPosition = transform.InverseTransformPoint(screenPoint);
+        offset = new Vector2(0, 100);
+        anchoredPosition = anchoredPosition + offset;
+        arrowUp = Instantiate(ArrowButton, anchoredPosition, Quaternion.identity);
+
+
+        arrowLeft.GetComponent<RectTransform>().Rotate(0, 0, 90);
+        arrowLeft.transform.parent = UI.transform;
+        arrowRight.GetComponent<RectTransform>().Rotate(0, 0, 270);
+        arrowRight.transform.parent = UI.transform;
+        arrowDown.GetComponent<RectTransform>().Rotate(0, 0, 180);
+        arrowDown.transform.parent = UI.transform;
+        arrowUp.transform.parent = UI.transform;
     }
 }
