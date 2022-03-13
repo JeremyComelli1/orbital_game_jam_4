b4efcb66efaa869184d6a9b0a5bdfbacfb41c6d9
Jeremy Comelli <jeremycomelli@bluewin.ch>
2022-03-13 09:42:06 +0100

    Adapted a few things

--- Assets/LevelImporter.cs
+++ Assets/LevelImporter.cs
@@ -4,25 +4,28 @@ using UnityEngine;
 
 public class LevelImporter : MonoBehaviour
 {
-    private const string levelsFolder = "../Assets/Levels/";
     //                      0         1     2      3        4               5         6     7
-    public enum tiles {CONCRETE = 0, DIRT, GRASS, SEED, SHEEP_CONCRETE, SHEEP_DIRT, GOAL, WATER};
+    public enum tiles {CONCRETE = 0, water, victory_water, SEED, SHEEP_CONCRETE, SHEEP_DIRT, GOAL, WATER};
 
     // Return the grid tiles
-    public static array GetTileMapFromFile(string fileName){
-        List<List<int>> grid = new  List<List<int>>();
+    public static List<List<string>> GetTileMapFromFile(string fileName){
+
+        List<List<string>> grid = new  List<List<string>>();
         
-        string[] lines = System.IO.File.ReadAllLines(String.Concat(levelsFolder + fileName););
+        string[] lines = System.IO.File.ReadAllLines(string.Concat(fileName));
+        int i = 0;
         foreach (string line in lines)
         {
-            int[] lineValues = line.Split(',');
-            List<int> gameTiles = new List<int>();
+            List<string> newLine = new List<string>();
 
-            for(int i = 0; i < lineValues.Length; i++){
-                gameTiles.add(lineValues[i]);
+            string[] lineValues = line.Split(',');
+
+
+            for(int j = 0; i < lineValues.Length; j++){
+                newLine.Add(lineValues[j]);
             }
 
-            grid.add(gameTiles);
+            grid.Add(newLine);
 
         }
 
